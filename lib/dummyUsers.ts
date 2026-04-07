3aa131f9137e00978e0b6e9393427ae69a5db4b5
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2026-04-07 20:37:49 +0600

    Ts error solved

--- lib/dummyUsers.ts
+++ lib/dummyUsers.ts
@@ -5,17 +5,20 @@ export const users: User[] = [
     id: "u1",
     name: "Alice",
     role: "admin",
+    email: "alice@example.com",
   },
 
   {
     id: "u2",
     name: "Bob",
     role: "manager",
+    email: "bob@example.com",
   },
 
   {
     id: "u3",
     name: "Charlie",
     role: "user",
+    email: "charlie@example.com",
   },
 ];
